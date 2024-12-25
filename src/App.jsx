434ff039e72bb3fefa732d4d488d072fb835fef5
hassangomaa <hassangomaa974@gmail.com>
2024-12-25 20:09:56 +0200

    Update Tailwind config, add site webmanifest, and enhance email submission logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
 import { BrowserRouter } from "react-router-dom";
-
 import {
-  About,
-  Contact,
-  Experience,
-  Hero,
   Navbar,
+  Hero,
+  About,
+  Experience,
   Tech,
   Works,
+  Contact,
   StarsCanvas,
 } from "./components";
 
+console.log("App.jsx: App component initialized");
+
 const App = () => {
+  console.log("App.jsx: Rendering App component");
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          {console.log("App.jsx: Rendering Navbar")}
           <Navbar />
+          {console.log("App.jsx: Rendering Hero")}
           <Hero />
         </div>
+        {console.log("App.jsx: Rendering About")}
         <About />
+        {console.log("App.jsx: Rendering Experience")}
         <Experience />
+        {console.log("App.jsx: Rendering Tech")}
         <Tech />
+        {console.log("App.jsx: Rendering Works")}
         <Works />
         <div className="relative z-0">
+          {console.log("App.jsx: Rendering Contact")}
           <Contact />
+          {console.log("App.jsx: Rendering StarsCanvas")}
           <StarsCanvas />
         </div>
       </div>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -44,83 +44,85 @@ const services = [
     title: "Back-End Development",
     icon: backend, // Replace with an appropriate icon
   },
-  {
-    title: "API Development & Integration",
-    icon: api, // Replace with an appropriate icon
-  },
-  {
-    title: "Database Design & Optimization",
-    icon: database, // Replace with an appropriate icon
-  },
-  {
-    title: "Microservices Architecture",
-    icon: microservices, // Replace with an appropriate icon
-  },
-  {
-    title: "Project Management & Mentorship",
-    icon: management, // Replace with an appropriate icon
-  },
+  // {
+  //   title: "API Development & Integration",
+  //   icon: api, // Replace with an appropriate icon
+  // },
+  // {
+  //   title: "Database Design & Optimization",
+  //   icon: database, // Replace with an appropriate icon
+  // },
+  // {
+  //   title: "Microservices Architecture",
+  //   icon: microservices, // Replace with an appropriate icon
+  // },
+  // {
+  //   title: "Project Management & Mentorship",
+  //   icon: management, // Replace with an appropriate icon
+  // },
 ];
 
 const technologies = [
-  {
-    name: "PHP",
-    icon: php, // Replace with an appropriate icon
-  },
   {
     name: "JavaScript",
     icon: javascript,
   },
   {
-    name: "TypeScript",
-    icon: typescript, // Replace with an appropriate icon
-  },
-  {
-    name: "Node.js",
-    icon: nodejs, // Replace with an appropriate icon
-  },
-  {
-    name: "Laravel",
-    icon: laravel, // Replace with an appropriate icon
-  },
-  {
-    name: "NestJS",
-    icon: nestjs, // Replace with an appropriate icon
-  },
-  {
-    name: "FastAPI",
-    icon: fastapi, // Replace with an appropriate icon
-  },
-  {
-    name: "MySQL",
-    icon: mysql, // Replace with an appropriate icon
-  },
-  {
-    name: "PostgreSQL",
-    icon: postgresql, // Replace with an appropriate icon
-  },
-  {
-    name: "GraphQL",
-    icon: graphql, // Replace with an appropriate icon
-  },
-  {
-    name: "Firebase",
-    icon: firebase, // Replace with an appropriate icon
-  },
-  {
     name: "Git",
     icon: git,
   },
-  {
-    name: "Docker",
-    icon: docker, // Replace with an appropriate icon
-  },
+  // {
+  //   name: "PHP",
+  //   icon: php, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "TypeScript",
+  //   icon: typescript, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "Node.js",
+  //   icon: nodejs, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "Laravel",
+  //   icon: laravel, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "NestJS",
+  //   icon: nestjs, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "FastAPI",
+  //   icon: fastapi, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "MySQL",
+  //   icon: mysql, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "PostgreSQL",
+  //   icon: postgresql, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "GraphQL",
+  //   icon: graphql, // Replace with an appropriate icon
+  // },
+  // {
+  //   name: "Firebase",
+  //   icon: firebase, // Replace with an appropriate icon
+  // },
+
+  // {
+  //   name: "Docker",
+  //   icon: docker, // Replace with an appropriate icon
+  // },
 ];
 const experiences = [
   {
     title: "Software Engineer",
     company_name: "SaladBar Vending Machine – KSA",
-    icon: saladBarIcon, // Replace with an appropriate icon
+    // icon: saladBarIcon, // Replace with an appropriate icon
+    icon: eliteSmile, // Replace with an appropriate icon
     iconBg: "#383E56",
     date: "Aug 2024 - Present",
     points: [
@@ -131,7 +133,9 @@ const experiences = [
   {
     title: "Software Engineer",
     company_name: "Misraj Technology – KSA",
-    icon: misrajIcon, // Replace with an appropriate icon
+    // icon: misrajIcon, // Replace with an appropriate icon
+    icon: eliteSmile, // Replace with an appropriate icon
+
     iconBg: "#eeeeee",
     date: "Aug 2023 - Jul 2024",
     points: [
@@ -143,7 +147,9 @@ const experiences = [
   {
     title: "Back End Developer",
     company_name: "Cord Digital – Egypt",
-    icon: cordDigitalIcon, // Replace with an appropriate icon
+    // icon: cordDigitalIcon, // Replace with an appropriate icon
+    icon: eliteSmile, // Replace with an appropriate icon
+
     iconBg: "#383E56",
     date: "Feb 2023 - Jul 2023",
     points: [
@@ -155,7 +161,9 @@ const experiences = [
   {
     title: "Back End Engineer",
     company_name: "Orange Digital Center – Egypt",
-    icon: orangeDigitalIcon, // Replace with an appropriate icon
+    // icon: orangeDigitalIcon, // Replace with an appropriate icon
+    icon: eliteSmile, // Replace with an appropriate icon
+
     iconBg: "#eeeeee",
     date: "Sep 2022 - Jan 2023",
     points: [
@@ -166,7 +174,9 @@ const experiences = [
   {
     title: "Full Stack Developer",
     company_name: "Freelance – Egypt",
-    icon: freelanceIcon, // Replace with an appropriate icon
+    // icon: freelanceIcon, // Replace with an appropriate icon
+    icon: eliteSmile, // Replace with an appropriate icon
+
     iconBg: "#383E56",
     date: "Jan 2022 - Present",
     points: [
@@ -204,69 +214,70 @@ const projects = [
         color: "green-text-gradient",
       },
     ],
-    image: muttonApp, // Replace with the project image
+    // image: muttonApp, // Replace with the project image
+    image: wadjet, // Replace with the project image
     source_code_link: "https://mtoun.com/", // Replace with the actual link
   },
-  {
-    name: "Fypi POS",
-    description: `
-      Built a POS system using PHP Laravel, Firebase, Stripe, and PayPal. Managed APIs, designed the database, and led the deployment of six integrated apps in an Agile-driven environment.
-    `,
-    tags: [
-      {
-        name: "php",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "laravel",
-        color: "green-text-gradient",
-      },
-      {
-        name: "firebase",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "stripe",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "paypal",
-        color: "green-text-gradient",
-      },
-    ],
-    image: fypiPOS, // Replace with the project image
-    source_code_link: "https://fypi.com", // Replace with the actual link
-  },
-  {
-    name: "Salad Bar Meals",
-    description: `
-      A vending machine integration project with PHP Filament, Laravel, Tailwind CSS, MySQL, REST APIs, microservices architecture, and web scraping across three applications.
-    `,
-    tags: [
-      {
-        name: "php",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "filament",
-        color: "green-text-gradient",
-      },
-      {
-        name: "laravel",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "mysql",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "tailwindcss",
-        color: "green-text-gradient",
-      },
-    ],
-    image: saladBar, // Replace with the project image
-    source_code_link: "https://saladbar.sa", // Replace with the actual link
-  },
+  // {
+  //   name: "Fypi POS",
+  //   description: `
+  //     Built a POS system using PHP Laravel, Firebase, Stripe, and PayPal. Managed APIs, designed the database, and led the deployment of six integrated apps in an Agile-driven environment.
+  //   `,
+  //   tags: [
+  //     {
+  //       name: "php",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "laravel",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "firebase",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "stripe",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "paypal",
+  //       color: "green-text-gradient",
+  //     },
+  //   ],
+  //   image: fypiPOS, // Replace with the project image
+  //   source_code_link: "https://fypi.com", // Replace with the actual link
+  // },
+  // {
+  //   name: "Salad Bar Meals",
+  //   description: `
+  //     A vending machine integration project with PHP Filament, Laravel, Tailwind CSS, MySQL, REST APIs, microservices architecture, and web scraping across three applications.
+  //   `,
+  //   tags: [
+  //     {
+  //       name: "php",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "filament",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "laravel",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "mysql",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "tailwindcss",
+  //       color: "green-text-gradient",
+  //     },
+  //   ],
+  //   image: saladBar, // Replace with the project image
+  //   source_code_link: "https://saladbar.sa", // Replace with the actual link
+  // },
 ];
 
 export { experiences, projects, services, technologies };
