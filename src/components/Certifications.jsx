d9989502aede341f2fd25ba548be64ee00867554
hassangomaa <hassangomaa974@gmail.com>
2026-05-27 05:01:51 +0300

    Fix: Tilt import in Certifications (default not named export)
    React error #130 was caused by { Tilt } named import instead of default import.
    Also moved options prop from inner div to Tilt component.

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Tilt } from "react-tilt";
+import Tilt from "react-tilt";
 
 import { styles } from "../styles";
 import { certifications } from "../constants";
@@ -8,17 +8,15 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const CertificationCard = ({ certification, index }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt
+      options={{ max: 45, scale: 1, speed: 450 }}
+      className="xs:w-[250px] w-full"
+    >
       <motion.div
         variants={fadeIn("up", "spring", index * 0.3, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
           className="bg-tertiary rounded-[20px] py-5 px-6 min-h-[120px] flex flex-col justify-between"
         >
           <div>
