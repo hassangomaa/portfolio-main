6647247186e3d839f8407785f631b0955b074e38
hassangomaa <hassangomaa974@gmail.com>
2026-05-27 05:09:42 +0300

    Fix: remove StrictMode, correct createRoot import, fix Tilt options prop
    - Remove React.StrictMode (causes findDOMNode warnings with react-tilt
      and double-render issues with Three.js canvases)
    - Use createRoot from react-dom/client (fixes deprecation warning)
    - Move options prop from inner div to Tilt in ServiceCard (About.jsx)
    - Remove console.log from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-console.log("main.jsx: ReactDOM rendering App component");
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
